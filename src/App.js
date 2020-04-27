04109b745c93c8e307f687379046e7feae5dc2ef
caotoanbkdt@gmail.com <caotoanbkdt@gmail.com>
2020-04-27 23:57:40 +0900

    use useEffect to call API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
-import TodoListItem from './components/todoList/todoListItem';
-import AddItem from './components/todoList/AddItem';
+import React, { useState, useEffect } from 'react';
+import PostList from './components/PostList';
+//import TodoListItem from './components/todoList/todoListItem';
+// import AddItem from './components/todoList/AddItem';
 
 function App() {
   const [todoList, setTodoList] = useState(() => {
     return JSON.parse(localStorage.getItem('list')) || ['Go School', 'Go To Office', 'Go To playfootball'];
   });
-
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    async function fetchPostLink() {
+      const requestUrl = "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1"
+      const response = await fetch(requestUrl);
+      const responseJson = await response.json();
+      const { data } = responseJson;
+      setPostList(data);
+    }
+    fetchPostLink();
+  }, [])
   function onSubmit(value) {
     const newList = [...todoList];
     newList.push(value);
@@ -21,10 +32,11 @@ function App() {
   }
   return (
     <div className="App">
-      <AddItem onSubmit={onSubmit} />
+      <PostList posts={postList} />
+      {/* <AddItem onSubmit={onSubmit} />
       <ul>
         {todoList.map((item, index) => <TodoListItem key={index} item={item} onToDeleteItem={() => onToDeleteItem(index)} />)}
-      </ul>
+      </ul> */}
     </div>
   );
 }
